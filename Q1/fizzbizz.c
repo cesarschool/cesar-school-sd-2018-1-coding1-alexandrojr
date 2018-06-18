83e6a8635540aaa3beeafb0598ee9c4537de4cf2
Alexandro Junior <40340156+alexandrojr@users.noreply.github.com>
2018-06-18 20:47:33 -0300

    Reposta Questão 1

--- Q1/fizzbizz.c
+++ Q1/fizzbizz.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
+/**
+* Implemente a função FizzBizz conforme definida abaixo. A mesma deve receber dois números inteiros não negativos (start e end) e imprimir a partir de start (incluído) até end (excluído) seguindo a mesma lógica do FizzBuzz.
+*
+* Função: void fizzBizz(int start, int end)
+* Input: inteiros (start < end) determinando os números de início e fim da contagem.
+* Output: utilize a função printf da biblioteca stdio.h e escreva um número ou palavra por linha (\n):
+*
+*/
+void fizzBizz(int start, int end)
+{
+  printf("\nFizzBizz::START: %d, %d\n", start, end);
+ 
+  // TODO: Your code here!
+  int numero = start;
+  
+  while( numero <= end)
+  {
+      if (numero % 15 == 0 ) 
+      {
+          printf("FizzBizz\n");
+      } 
+    
+    else if(numero % 3 == 0) 
+    {
+          printf("Fizz\n"); // Printa os divisiveis por 3
+     } 
+    
+    else if(numero % 5 == 0) 
+    {
+          printf("Bizz\n");  // Printa os divisiveis por 5
+      } 
+    else 
+    {
+          printf("%d\n", numero);
+     }
+      numero++; //soma +1
+  }
+ 
+  // Ex.: printf("Fizz\n");
+ 
+  printf("\nFizzBizz::END\n");
+}
+
+int main()
+{
+  printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: FizzBizz ###\n");
+ 
+  fizzBizz(1, 6);
+  fizzBizz(1, 16);
+  fizzBizz(1, 100);
+ 
+  return 0;
+}
 
